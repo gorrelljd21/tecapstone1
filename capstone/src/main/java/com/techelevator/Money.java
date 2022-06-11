3ebb3e1e2bafb262b3239d6492a9d4537af9a02c
Evan Amezcua <ema9701@gmail.com>
2022-06-11 16:02:24 -0400

    Capstone_VM_change_fx_moddified

--- capstone/src/main/java/com/techelevator/Money.java
+++ capstone/src/main/java/com/techelevator/Money.java
@@ -1,15 +1,16 @@
 package com.techelevator;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Money {
 
-    BigDecimal quarter = BigDecimal.valueOf(0.25);
-    BigDecimal dime = BigDecimal.valueOf(0.10);
-    BigDecimal nickel = BigDecimal.valueOf(0.05);
+//    BigDecimal quarter = BigDecimal.valueOf(0.25);
+//    BigDecimal dime = BigDecimal.valueOf(0.10);
+//    BigDecimal nickel = BigDecimal.valueOf(0.05);
 
 
     private BigDecimal balance = new BigDecimal("0.00");
@@ -20,26 +21,44 @@ public class Money {
 
     public void makeChange(BigDecimal balance){
 
-        BigDecimal quartersReturned = balance.divideToIntegralValue(quarter);
-        BigDecimal qR = quartersReturned.divide(quarter);
-        BigDecimal dimesReturned = quartersReturned.subtract(quarter.multiply(BigDecimal.valueOf(qR.intValue())));
-        BigDecimal dR = dimesReturned.divide(dime);
-        BigDecimal nicklesReturned = dimesReturned.subtract(dime.multiply(BigDecimal.valueOf(dR.intValue())));
-        BigDecimal nR = nicklesReturned.divide(nickel);
-
-
-        //        double change = 0.00;
+//        BigDecimal quartersReturned = balance.divideToIntegralValue(quarter);
+//        BigDecimal qR = quartersReturned.divide(quarter);
+//        BigDecimal dimesReturned = quartersReturned.subtract(quarter.multiply(BigDecimal.valueOf(qR.intValue())));
+//        BigDecimal dR = dimesReturned.divide(dime);
+//        BigDecimal nicklesReturned = dimesReturned.subtract(dime.multiply(BigDecimal.valueOf(dR.intValue())));
+//        BigDecimal nR = nicklesReturned.divide(nickel);
+//
+//        double change = balance.doubleValue();
+//        change = change * 100;
 //
 //        int quarters = (int)(change/25);
 //        quarters = quarters % 25;
 //
-//        int dimes = (int)(change/10);
-//        dimes= dimes % 10;
+//        int dimes = quarters - (int)(change/10);
+//        dimes = dimes % 10;
 //
-//        int nickles = (int)(change/5);
+//        int nickles = dimes - (int)(change/5);
 //        nickles = nickles % 5;
+        int change = balance.multiply(BigDecimal.valueOf(100)).intValue();
+        int quarters = 0;
+        int dimes = 0;
+        int nickels = 0;
+//
+        while (change >= 25) {
+            quarters += 1;
+            change -= 25;
+        }
+        while (change >= 10) {
+            dimes += 1;
+            change -= 10;
+            while (change >= 5) {
+                nickels += 1;
+                change -= 5;
+            }
+        }
 
-        System.out.printf("Your change is: %s quarters, %s dimes, and %s nickles %n", quartersReturned.intValue(), dR.intValue(), nR.intValue());
+
+        System.out.printf("Your change is: %s quarters, %s dimes, and %s nickles %n", quarters, dimes, nickels);
 
     }
 
